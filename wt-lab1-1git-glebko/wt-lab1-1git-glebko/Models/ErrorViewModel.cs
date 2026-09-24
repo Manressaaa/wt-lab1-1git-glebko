63e88d38eb62a25c37abf23a2a6dcd068a384552
Дарья Глебко <dashaglebko34@gmail.com>
2026-09-24 17:02:12 +0300

    feat: add net core logic

---
+++ wt-lab1-1git-glebko/wt-lab1-1git-glebko/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wt_lab1_1git_glebko.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
